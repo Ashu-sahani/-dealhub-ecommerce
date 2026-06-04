3509990a512c29980692bd8b63a8a5b7c110da85
Ashu-sahani <ashusahani021@gmail.com>
2026-06-04 18:01:38 +0530

    Create Cart.vue

---
+++ frontend/src/frontend/src/router/frontend/src/views/frontend/src/views/Cart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Cart</h2>
+    <ul>
+      <li v-for="(item,i) in cartStore.items" :key="i">{{ item.name }} - ${{ item.price }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useCartStore } from '../stores/cart.js'
+const cartStore = useCartStore()
+</script>
