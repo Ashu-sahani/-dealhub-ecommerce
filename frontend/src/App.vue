aec894e6e2df253fc087e8b473f3788c2b411333
Ashu-sahani <ashusahani021@gmail.com>
2026-06-04 17:59:41 +0530

    Add main layout for DealHub application

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav>
+      <h1>DealHub</h1>
+      <router-link to="/">Products</router-link>
+      <router-link to="/cart">Cart ({{ cartStore.items.length }})</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import { useCartStore } from './stores/cart.js'
+const cartStore = useCartStore()
+</script>
